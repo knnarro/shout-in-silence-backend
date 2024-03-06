1951e626fc996a903f307e3cdd1724d087bc5273
Naro <knnarro@gmail.com>
2024-03-07 00:50:13 +0900

    refactor: 코드를 조금 더 가독성 좋게 수정합니다.

--- core/websocket_manager.py
+++ core/websocket_manager.py
@@ -28,21 +28,15 @@ class WebsocketManager:
         logger.info(f"Websocket disconnected {websocket}")
 
     async def broadcast(self, message: str):
+        await self.pubsub_client.redis_connection.incr("count")
         await self.pubsub_client.publish("shout", message)
-        count = await self.pubsub_client.redis_connection.get("count")
-        if not isinstance(count, bytes):
-            await count
-        await self.pubsub_client.redis_connection.set(
-            "count", int(count.decode("utf-8")) + 1
-        )
 
     async def _listen_and_send(self, subscriber, websocket):
         while True:
             message = await subscriber.get_message(ignore_subscribe_messages=True)
             if message is not None:
                 count = await self.pubsub_client.redis_connection.get("count")
-                if not isinstance(count, bytes):
-                    await count
+                count = int(count) if count else 0
                 body = json.loads(message["data"].decode("utf-8"))
-                body["count"] = int(count.decode("utf-8"))
+                body["count"] = count
                 await websocket.send_text(json.dumps(body))
